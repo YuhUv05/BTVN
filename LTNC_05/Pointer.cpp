817cb042ba8e652965183667c100d84fcfae97c5
YuhUv05 <164222173+YuhUv05@users.noreply.github.com>
2024-05-02 10:15:14 +0700

    Create Pointer.cpp

---
+++ LTNC_05/Pointer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void update(int *a ,int *b) {
+    int tmp = *a;
+    *a = *a + *b;
+    *b = abs(tmp - *b);
+}
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
